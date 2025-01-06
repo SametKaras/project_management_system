6284f8ccb5e76d0375f48f43d04e6f7dcd1d55a1
SametKaras <sametkaras.tr@gmail.com>
2025-01-06 18:57:10 +0300

    feat: necessary shadcn components added and modified.

--- project-management-system/src/app/page.tsx
+++ project-management-system/src/app/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click Me</Button>
+    <div className="flex gap-4">
+      <Input />
+      <Button size="lg"> Primary </Button>
+      <Button variant="secondary">Secondary </Button>
+      <Button variant="destructive">Destructive </Button>
+      <Button variant="ghost">Ghost </Button>
+      <Button variant="muted">Muted </Button>
+      <Button variant="outline">Outline </Button>
+      <Button variant="teritary">teritary </Button>
     </div>
   );
 }
