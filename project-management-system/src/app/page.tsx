8290fc16b2529edd7d5b899355c6462d68e03c68
SametKaras <sametkaras.tr@gmail.com>
2025-01-07 19:46:50 +0300

    feat: session middleware built

--- project-management-system/src/app/page.tsx
+++ project-management-system/src/app/page.tsx
@@ -1,17 +1,26 @@
+"use client";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
-    <div className="flex gap-4">
-      <Input />
-      <Button size="lg"> Primary </Button>
-      <Button variant="secondary">Secondary </Button>
-      <Button variant="destructive">Destructive </Button>
-      <Button variant="ghost">Ghost </Button>
-      <Button variant="muted">Muted </Button>
-      <Button variant="outline">Outline </Button>
-      <Button variant="teritary">teritary </Button>
+    <div>
+      Sadece izinli kişilere görünür
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
