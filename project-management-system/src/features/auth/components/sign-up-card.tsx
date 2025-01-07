08abb3d03e2e251b311da3d1318f4e0904df5b55
SametKaras <sametkaras.tr@gmail.com>
2025-01-07 13:49:51 +0300

    feat: auth screens created

---
+++ project-management-system/src/features/auth/components/sign-up-card.tsx
@@ -0,0 +1,160 @@
+import { z } from "zod";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  name: z.string().trim().min(1, "Gerekli"),
+  email: z.string().email(),
+  password: z.string().min(8, "Minimum 8 Karakter Gerekli"),
+});
+
+export const SignUpCard = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log({ values });
+  };
+
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Kayıt Ol</CardTitle>
+        <CardDescription>
+          Kayıt olarak{" "}
+          <Link href="/privacy">
+            <span className="text-blue-700">Gizlilik Politikası</span>
+          </Link>{" "}
+          ve{" "}
+          <Link href="/terms">
+            <span className="text-blue-700">Hizmet Şartlarını</span>
+          </Link>{" "}
+          kabul etmiş olursunuz.
+        </CardDescription>
+      </CardHeader>
+
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      placeholder="İsminizi giriniz"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                      placeholder="Email adresinizi giriniz"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="password"
+                      placeholder="Şifrenizi giriniz"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button disabled={false} size="lg" className="w-full">
+              Giriş Yap
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full"
+        >
+          <FcGoogle className="mr-2 size-5" />
+          Google İle Giriş Yap
+        </Button>
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full"
+        >
+          <FaGithub className="mr-2 size-5" />
+          Github İle Giriş Yap
+        </Button>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator></DottedSeparator>
+      </div>
+      <CardContent className="p-7 flex items-center justify-center">
+        <p>Zaten bir hesabınız var mı ?</p>
+        <Link href={"/sign-in"}>
+          <span className="text-blue-700">&nbsp; Giriş Yap</span>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+};
