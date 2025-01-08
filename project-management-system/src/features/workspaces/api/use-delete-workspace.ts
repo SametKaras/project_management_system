47c54ee9b22befc48204956f4286333a89e142dd
SametKaras <sametkaras.tr@gmail.com>
2025-01-08 23:12:49 +0300

    feat: delete function added

---
+++ project-management-system/src/features/workspaces/api/use-delete-workspace.ts
@@ -0,0 +1,41 @@
+import { toast } from "sonner";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/rpc";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.workspaces)[":workspaceId"]["$delete"],
+  200
+>;
+
+type RequestType = InferRequestType<
+  (typeof client.api.workspaces)[":workspaceId"]["$delete"]
+>;
+
+export const useDeleteWorkspace = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param }) => {
+      const response = await client.api.workspaces[":workspaceId"]["$delete"]({
+        param,
+      });
+
+      if (!response.ok) {
+        throw new Error("Çalışma alanı silme başarısız");
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success("Çalışma alanı kaldırıldı");
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
+      queryClient.invalidateQueries({ queryKey: ["workspace", data.$id] });
+    },
+    onError: () => {
+      toast.error("Çalışma alanı kaldırılamadı");
+    },
+  });
+  return mutation;
+};
