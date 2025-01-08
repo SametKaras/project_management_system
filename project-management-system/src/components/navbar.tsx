29fa2d706318583ff98d0ada7974110e86d35683
SametKaras <sametkaras.tr@gmail.com>
2025-01-08 12:24:25 +0300

    feat: dashboard layout created

---
+++ project-management-system/src/components/navbar.tsx
@@ -0,0 +1,18 @@
+import { UserButton } from "@/features/auth/components/user-button";
+import { MobileSidebar } from "./mobile-sidebar";
+
+export const Navbar = () => {
+  return (
+    <nav className="pt-4 px-6 flex items-center justify-between">
+      <div className="flex-col hidden lg:flex">
+        <h1 className="text-2xl font-semibold">Ana Sayfa </h1>
+        <p className="text-muted-foreground">
+          {" "}
+          Bütün projelerini ve görevlerini görüntüle.
+        </p>
+      </div>
+      <MobileSidebar />
+      <UserButton />
+    </nav>
+  );
+};
