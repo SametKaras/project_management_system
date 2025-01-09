1f399912dfc9467000d891202544a3a1f21888f4
SametKaras <sametkaras.tr@gmail.com>
2025-01-09 13:57:26 +0300

    feat: invite system built

---
+++ project-management-system/src/features/workspaces/api/use-join-workspace.ts
@@ -0,0 +1,44 @@
+import { toast } from "sonner";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/rpc";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.workspaces)[":workspaceId"]["join"]["$post"],
+  200
+>;
+
+type RequestType = InferRequestType<
+  (typeof client.api.workspaces)[":workspaceId"]["join"]["$post"]
+>;
+
+export const useJoinWorkspace = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ param, json }) => {
+      const response = await client.api.workspaces[":workspaceId"]["join"][
+        "$post"
+      ]({
+        param,
+        json,
+      });
+
+      if (!response.ok) {
+        throw new Error("Çalışma alanına katılma başarısız");
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data }) => {
+      toast.success("Çalışma alanına katılındı");
+      queryClient.invalidateQueries({ queryKey: ["workspaces"] });
+      queryClient.invalidateQueries({ queryKey: ["workspace", data.$id] });
+    },
+    onError: () => {
+      toast.error("Çalışma alanına katılma başarısız");
+    },
+  });
+  return mutation;
+};
---
+++ project-management-system/src/features/workspaces/components/join-workspace-form.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import Link from "next/Link";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useJoinWorkspace } from "../api/use-join-workspace";
+import { useInviteCode } from "../hooks/use-invite-code";
+import { useWorkspaceId } from "../hooks/use-workspace-id";
+import { useRouter } from "next/navigation";
+
+interface JoinWorkspaceFormProps {
+  initialValues: {
+    name: string;
+  };
+}
+
+export const JoinWorkspaceForm = ({
+  initialValues,
+}: JoinWorkspaceFormProps) => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+  const inviteCode = useInviteCode();
+  const { mutate, isPending } = useJoinWorkspace();
+
+  const onSubmit = () => {
+    mutate({
+      param: { workspaceId },
+      json: {
+        code: inviteCode,
+      },
+      onSuccess: ({ data }) => {
+        router.push(`/workspaces/${data.$id}`);
+      },
+    });
+  };
+
+  return (
+    <Card className="w-full h-full border-none shadow-none">
+      <CardHeader className="p-7">
+        <CardTitle className="text-xl font-bold">
+          Çalışma alanına katıl
+        </CardTitle>
+        <CardDescription>
+          <strong> {initialValues.name}</strong> isimli çalışma alanına
+          katılmaya davet edildiniz.
+        </CardDescription>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <div className="flex flex-col lg:flex-row gap-2 items-center justify-between">
+          <Button
+            className="w-full lg:w-fit"
+            variant="secondary"
+            type="button"
+            asChild
+            size="lg"
+            disabled={isPending}
+          >
+            <Link href="/">İptal</Link>
+          </Button>
+          <Button
+            size="lg"
+            type="button"
+            className="w-full lg:w-fit"
+            onClick={onSubmit}
+            disabled={isPending}
+          >
+            Onayla
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
